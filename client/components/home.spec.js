571cbff09b34eef1df6050f46848cc81dff64225
Bobby Lux <robert.michael.lux@gmail.com>
2018-03-16 17:48:12 -0400

    test: change user-home.spec.js to home.spec.js placeholder

--- client/components/home.spec.js
+++ client/components/home.spec.js
@@ -4,7 +4,7 @@ import {expect} from 'chai'
 import React from 'react'
 import enzyme, {shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import {UserHome} from './user-home'
+import {Home} from './home'
 
 const adapter = new Adapter()
 enzyme.configure({adapter})
@@ -13,10 +13,10 @@ describe('UserHome', () => {
   let userHome
 
   beforeEach(() => {
-    userHome = shallow(<UserHome email={'cody@email.com'} />)
+    userHome = shallow(<Home />)
   })
 
-  it('renders the email in an h3', () => {
-    expect(userHome.find('h3').text()).to.be.equal('Welcome, cody@email.com')
+  it('renders *Home* as an h1', () => {
+    expect(userHome.find('h1').text()).to.be.equal('Home')
   })
 })
