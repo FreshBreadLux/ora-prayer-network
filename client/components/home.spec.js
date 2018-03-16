ddd7debfe813730adc619df379ae63271ed2ab5c
Bobby Lux <robert.michael.lux@gmail.com>
2018-03-16 17:49:37 -0400

    fix: change import syntax for Home component in home.spec.js

--- client/components/home.spec.js
+++ client/components/home.spec.js
@@ -4,19 +4,19 @@ import {expect} from 'chai'
 import React from 'react'
 import enzyme, {shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import {Home} from './home'
+import Home from './home'
 
 const adapter = new Adapter()
 enzyme.configure({adapter})
 
-describe('UserHome', () => {
-  let userHome
+describe('home', () => {
+  let home
 
   beforeEach(() => {
-    userHome = shallow(<Home />)
+    home = shallow(<Home />)
   })
 
   it('renders *Home* as an h1', () => {
-    expect(userHome.find('h1').text()).to.be.equal('Home')
+    expect(home.find('h1').text()).to.be.equal('Home')
   })
 })
