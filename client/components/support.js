bac7656bb620a89cc8b1edb1f0511f712bad36cb
Bobby Lux <robert.michael.lux@gmail.com>
2018-03-18 22:29:58 -0400

    feat: build out form on support.js

--- client/components/support.js
+++ client/components/support.js
@@ -11,10 +11,27 @@ class Support extends React.Component {
       body: ''
     }
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleName = this.handleName.bind(this)
+    this.handleEmail = this.handleEmail.bind(this)
+    this.handleSubject = this.handleSubject.bind(this)
+    this.handleBody = this.handleBody.bind(this)
   }
 
   handleSubmit(event) {
     event.preventDefault()
+  }
+
+  handleName(event) {
+    this.setState({ name: event.target.value })
+  }
+  handleEmail(event) {
+    this.setState({ email: event.target.value })
+  }
+  handleSubject(event) {
+    this.setState({ subject: event.target.value })
+  }
+  handleBody(event) {
+    this.setState({ body: event.target.value })
   }
 
   render() {
@@ -24,10 +41,10 @@ class Support extends React.Component {
         <text className="raleway font20">Need to tell us about a bug?</text>
         <text className="raleway font20">Want to share your story?</text>
         <form onSubmit={this.handleSubmit}>
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
+          <input type="text" value={this.state.name} onChange={this.handleName} />
+          <input type="text" value={this.state.email} onChange={this.handleEmail} />
+          <input type="text" value={this.state.subject} onChange={this.handleSubject} />
+          <input type="text" value={this.subject.body} onChange={this.handleBody} />
         </form>
       </div>
     )
